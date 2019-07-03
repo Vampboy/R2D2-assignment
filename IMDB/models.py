6c8ae464a287a42437c79a7415fd765968490b5a
Light <prakashnarasanagi22@gmail.com>
2019-07-04 02:57:56 +0530

    working autocomplete

--- IMDB/models.py
+++ IMDB/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class imdb(models.Model):
+    id = models.IntegerField(primary_key=True)
+    moviename = models.CharField(max_length=100,default=' ')
+    raing = models.CharField(max_length=10,default=' ')
+    star_cast = models.CharField(max_length=300,default=' ')
+
+    class Meta:
+        db_table = 'imdb'
+
+    def __str__(self):
+        return self.moviename
--- IMDB/views.py
+++ IMDB/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render , render_to_response
 from django.http import HttpResponse
+from .models import imdb
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request,'index.html',{})
+
+
+#from django.utils import simplejson
+#def autocompleteModel(request):
+#    search_qs = ModelName.objects.filter(name__startswith=request.REQUEST['search'])
+#    results = []
+#    for r in search_qs:
+#        results.append(r.name)
+#    resp = request.REQUEST['callback'] + '(' + simplejson.dumps(result) + ');'
+#    return HttpResponse(resp, content_type='application/json')
+
+def search_titles(request):
+    if request.method == 'POST':
+        search_text = request.POST['search_text']
+    else:
+        search_text = ''
+    #print(imdb.objects.all())
+    data = imdb.objects.order_by('raing').filter(moviename__icontains=str(search_text))[:5]
+    print(data)
+
+    articles = data
+    return render_to_response('ajax_search.html',{'articles':articles})
+
+def result(request,num=0):
+    data = imdb.objects.get(id=num)
+    print(data.id,data.moviename)
+    return HttpResponse("hello")
